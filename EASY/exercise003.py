d11d954891c0543d8da6126ffc5019993056766d
tevooshw <cstzzd@gmail.com>
2025-02-01 23:12:36 -0300

    Exercise 1-3.

--- EASY/exercise003.py
+++ EASY/exercise003.py
@@ -8,11 +8,7 @@ Description exercise 3: Write a program that reads an integer and calculates the
 # Code for this question
 user_input = int(input("Enter a number here: "))
 sum_numbers = 0
-numbers = 1
-numbers_list = []  
 while sum_numbers < user_input:
-    sum_numbers += numbers
-    numbers_list.append(numbers)
-    numbers += 1
+    sum_numbers += 1
+    print(f"You choose the number {user_input} and we are in the number {sum_numbers}")
 
-print(f"You choose the number {sum_numbers} and the way for this is: ({' + '.join(map(str, numbers_list))})")
